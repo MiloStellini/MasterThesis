d506779efd5cea566d2a6bc8716c856f642f1541
MiloStellini <milostellini19@gmail.com>
2026-02-02 09:07:39 +0100

    Update DiscreteRMP.py

--- DiscreteRMP.py
+++ DiscreteRMP.py
@@ -30,7 +30,7 @@ class DiscreteRMP:
             for col in column:
                 col.var.setAttr(GRB.Attr.VType, GRB.INTEGER)
 
-    def run(self, timelimit : int = 600 ):
+    def run(self, timelimit : int = 600000):
         
         self.model.setParam('TimeLimit', timelimit)
 
